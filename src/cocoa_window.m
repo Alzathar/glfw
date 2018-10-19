7cd7f36b6ffc97a2e66fec5ea301b0102ddc88ae
Arnaud Barré <arnaud.barre@gmail.com>
2018-10-19 00:21:27 -0400

    NSControlKeyMask is deprecated in macOS 10.12 and has to be replaced by NSEventModifierFlagControl.

--- src/cocoa_window.m
+++ src/cocoa_window.m
@@ -587,7 +587,7 @@ static const NSRange kEmptyRange = { NSNotFound, 0 };
     {
         const NSUInteger modifierFlags = [nsevent modifierFlags];
         NSString *chs = [nsevent charactersIgnoringModifiers];
-        if (modifierFlags & NSControlKeyMask)
+        if (modifierFlags & NSEventModifierFlagControl)
         {
             if ([chs characterAtIndex:0] == NSTabCharacter)
             {
